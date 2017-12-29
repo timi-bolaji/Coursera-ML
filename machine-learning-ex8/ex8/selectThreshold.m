e60c5350f0d0091343b4d851983a0dacc6237d8f
Timi Bolaji <bolajitimilehin@gmail.com>
2017-12-29 19:12:52 +0100

    Part 1 - Anomaly Detection done

--- machine-learning-ex8/ex8/selectThreshold.m
+++ machine-learning-ex8/ex8/selectThreshold.m
@@ -24,8 +24,12 @@ for epsilon = min(pval):stepsize:max(pval)
     %       of 0's and 1's of the outlier predictions
 
 
+    predictions = pval < epsilon;
+    tp = sum(predictions.*yval);
+    fp = sum(predictions.*(1-yval));
+    fn = sum((1-predictions).*yval);
 
-
+    F1 = (2*tp) / (tp + tp + fp + fn);
 
 
 
